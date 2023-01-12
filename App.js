ff3df50db7ae578ee248465e537c46327d0a7708
Daniel Yol <danyhiol@googlemail.com>
2023-01-12 11:52:05 +0100

    first commit

--- App.js
+++ App.js
@@ -2,11 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { Tesseract } from './src/components/tesseract';
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Welcome to React-Native (expo) tesseract</Text>
       <StatusBar style="auto" />
+      <Tesseract />
     </View>
   );
 }
